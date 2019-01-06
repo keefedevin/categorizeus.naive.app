bf30af7d361f6f0d91169eb00107428626d976e6
keefe <keefe@categorize.us>
2019-01-06 10:36:28 -0800

    streamline configuration

--- src/main/java/us/categorize/naive/app/Config.java
+++ src/main/java/us/categorize/naive/app/Config.java
@@ -1,34 +1,62 @@
 package us.categorize.naive.app;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import us.categorize.CategorizeUs;
+import us.categorize.naive.NaiveMessageStore;
+import us.categorize.naive.NaiveUserStore;
+import us.categorize.naive.api.NaiveAuthorizer;
 
 public class Config {
 	public static final int DEFAULT_PAGE_SIZE = 10, DEFAULT_PAGE_ON = 0;
-	private String clearSql, createSql, dbHost, dbPort, dbName, dbUser, dbPass, staticDir, indexSql, seedSql, fileBase;
+	private String staticDir="~/projects/categorizeus.naive.app/src/main/resources/static";
+	private String fileBase="~/projects/files/";
+	private String secretsFile = "~/projects/secrets/secrets.properties";//unclear is ~/ will really work
+	private String googleClientId, googleClientSecret;
+	private String dbHost, dbPort, dbName, dbUser, dbPass;
 	private double maxThumbWidth, maxThumbHeight;
 	private long maxUploadSize = -1;
 	private int port;
 	private String attachmentURLPrefix, connectString;
 	private String driverName = "org.postgresql.Driver";
-
-	public static Config readRelativeConfig() throws Exception {
-		Properties properties = new Properties();
-		InputStream input = Config.class.getResourceAsStream("/categorizeus.properties");
-		properties.load(input);
-		StringWriter writer = new StringWriter();
-		properties.list(new PrintWriter(writer));
-		System.out.println("Properties File Read As ");
-		System.out.println(writer.getBuffer().toString());
-		Config config = new Config(properties);
-		return config;
+	
+	public static String toLocal(String linuxPath) {
+		//converts a linux style path to local absolute path
+		String toHome = linuxPath.replace("~/", System.getProperty("user.home")+File.separator);
+		toHome = toHome.replace('/', File.separatorChar);
+		System.out.println(toHome);
+		return toHome;
+	}
+	public static void overrideProperties(Properties properties, Properties overrideProperties) {
+		for(String secretProperty : overrideProperties.stringPropertyNames()) {
+			properties.setProperty(secretProperty, overrideProperties.getProperty(secretProperty));
+		}
 	}
 
+	
 	public Config(Properties properties) {
+		this.staticDir = toLocal(this.staticDir);
+		this.fileBase = toLocal(this.fileBase);
+		this.secretsFile = toLocal(this.secretsFile);
+		
+		String secretsFile = properties.getProperty("SECRETS_DIR");
+		if(!"".equals(secretsFile)) this.secretsFile = secretsFile;
+		overrideProperties(properties);
+		
+		String staticDir = properties.getProperty("STATIC_DIR");
+		if(!"".equals(staticDir)) this.staticDir = staticDir;
+		String fileBase = properties.getProperty("FILE_BASE");
+		if(!"".equals(fileBase)) this.fileBase = fileBase;
+		googleClientId = properties.getProperty("GOOGLE_CLIENT_ID");
+		googleClientSecret = properties.getProperty("GOOGLE_CLIENT_SECRET");
 		dbName = properties.getProperty("DB_NAME");
 		dbHost = properties.getProperty("DB_HOST");
 		dbPort = properties.getProperty("DB_PORT");
@@ -41,31 +69,41 @@ public class Config {
 		maxThumbWidth = Double.parseDouble(properties.getProperty("MAX_THUMB_WIDTH"));
 		maxThumbHeight = Double.parseDouble(properties.getProperty("MAX_THUMB_HEIGHT"));
 		port = Integer.parseInt(properties.getProperty("PORT"));
-		staticDir = properties.getProperty("STATIC_DIR");
+	}
+	
+	//everything about this method is cringe
+	public void configureCategorizeUs() throws ClassNotFoundException, SQLException {
+		//clearly does not belong here
+		CategorizeUs.instance().setGoogleClientId(this.googleClientId);
+		CategorizeUs.instance().setGoogleClientSecret(this.googleClientSecret);
+
+		//TODO note the annoying ordering dependency here
+		CategorizeUs.instance().setUserStore(new NaiveUserStore(getDatabaseConnection()));
+		CategorizeUs.instance().setMessageStore(new NaiveMessageStore(getDatabaseConnection(), CategorizeUs.instance().getUserStore(), getFileBase()));
+		CategorizeUs.instance().setAuthorizer(new NaiveAuthorizer(CategorizeUs.instance().getUserStore()));
+	}
+	private void overrideProperties(Properties properties) {
+		File file = new File(this.secretsFile);
+		if(file!=null && file.exists() && !file.isDirectory()) {
+			Properties secretProperties = new Properties();
+			InputStream input;
+			try {
+				input = new FileInputStream(file);
+				secretProperties.load(input);
+				overrideProperties(properties, secretProperties);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
-	public Connection getDatabaseConnection() throws Exception {
-		Class.forName("org.postgresql.Driver");
+	public Connection getDatabaseConnection() throws SQLException, ClassNotFoundException {
+		Class.forName(driverName);
 		System.out.println("Postgres Driver Loaded");
 		System.out.println("Connecting " + getConnectString() + "," + getDbUser() + "," + getDbPass());
 		Connection conn = DriverManager.getConnection(getConnectString(), getDbUser(), getDbPass());
 		return conn;
-	}
-
-	public String getClearSql() {
-		return clearSql;
-	}
-
-	public void setClearSql(String clearSql) {
-		this.clearSql = clearSql;
-	}
-
-	public String getCreateSql() {
-		return createSql;
-	}
-
-	public void setCreateSql(String createSql) {
-		this.createSql = createSql;
 	}
 
 	public String getDbHost() {
@@ -114,22 +152,6 @@ public class Config {
 
 	public void setStaticDir(String staticDir) {
 		this.staticDir = staticDir;
-	}
-
-	public String getIndexSql() {
-		return indexSql;
-	}
-
-	public void setIndexSql(String indexSql) {
-		this.indexSql = indexSql;
-	}
-
-	public String getSeedSql() {
-		return seedSql;
-	}
-
-	public void setSeedSql(String seedSql) {
-		this.seedSql = seedSql;
 	}
 
 	public String getFileBase() {
@@ -195,5 +217,22 @@ public class Config {
 	public void setDriverName(String driverName) {
 		this.driverName = driverName;
 	}
-
+	public String getSecretsFile() {
+		return secretsFile;
+	}
+	public void setSecretsFile(String secretsFile) {
+		this.secretsFile = secretsFile;
+	}
+	public String getGoogleClientId() {
+		return googleClientId;
+	}
+	public void setGoogleClientId(String googleClientId) {
+		this.googleClientId = googleClientId;
+	}
+	public String getGoogleClientSecret() {
+		return googleClientSecret;
+	}
+	public void setGoogleClientSecret(String googleClientSecret) {
+		this.googleClientSecret = googleClientSecret;
+	}
 }
