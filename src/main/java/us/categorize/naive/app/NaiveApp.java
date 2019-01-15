7db6eba007610da687686063f1a9581afdbc9f27
keefe <keefe@categorize.us>
2019-01-15 12:27:33 -0800

    update docs text

--- src/main/java/us/categorize/naive/app/NaiveApp.java
+++ src/main/java/us/categorize/naive/app/NaiveApp.java
@@ -9,11 +9,6 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
 
-import us.categorize.CategorizeUs;
-import us.categorize.naive.NaiveMessageStore;
-import us.categorize.naive.NaiveUserStore;
-import us.categorize.naive.api.NaiveAuthorizer;
-
 public class NaiveApp {
 
 	public static void main(String[] args) throws Exception{
